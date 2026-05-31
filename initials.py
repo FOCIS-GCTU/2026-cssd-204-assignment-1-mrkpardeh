39c2be314c153346359b14e92d9f3ab659c3fa42
mrkpardeh <2425402792@live.gctu.edu.gh>
2026-05-31 19:02:02 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+# File: initials.py
+# Description: Prints stylized large initials PGK with surrounding periods.
+# Assignment Number: 1
+#
+# Name: Paul Gwezeoh Kpardeh
+# STUDENT ID: 2425402792
+# Email: 2425402792@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Paul Gwezeoh Kpardeh, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    """Prints the small initials and stylized large initials for PGK."""
+    print()
+
+    print("...PGK")
+
+    print()
+
+    print("...PPPPPPPPPP...........GGGGGGGGGG.........KK........KK.....")
+    print("...PPPPPPPPPPPP........GGGGGGGGGGGG........KK......KK.......")
+    print("...PP........PP........GG..................KK....KK.........")
+    print("...PP........PP........GG..................KK..KK...........")
+    print("...PPPPPPPPPPPP........GG.....GGGGG........KKKK.............")
+    print("...PPPPPPPP............GG.....GGGGG........KKKK.............")
+    print("...PP..................GG........GG........KK..KK...........")
+    print("...PP..................GG........GG........KK....KK.........")
+    print("...PP.............**...GGGGGGGGGGGG...**...KK......KK.....**")
+    print("...PP.............**....GGGGGGGGGG....**...KK........KK...**")
+
+    print()
 
 
-
-main()
+if __name__ == "__main__":
+    main()
